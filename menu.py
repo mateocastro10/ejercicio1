62bde8af99553cf8be7fb38f9ee9ed9cbc14fc9a
mateocastro10 <103914828+mateocastro10@users.noreply.github.com>
2022-04-25 15:58:23 -0300

    Add files via upload

---
+++ menu.py
@@ -0,0 +1,34 @@
+from import
+
+class Menu:
+    __op=None
+    def __init__(self):
+        self.__op = { '1':self.cantidadTotalMillas,
+                            '2':self.acumularmillas,
+                            '3':self.canjear,
+                            '4':self.salir
+                          }
+    def opcion(self,op,manejadorRP,manejadorEP):
+        func=self.__switcher.get(op, lambda: print("Opción no válida"))
+        if op=='1' or op=='2':
+            func(manejadorRP,manejadorEP)
+        else:
+            func()
+    def salir(self):
+        print('Usted salio del programa')
+    def opcion1(self,manejadorRP,manejadorEP):
+        if type(manejadorRP)==ManejadorRecepcionProducto and type(manejadorEP)==ManejadorEntregaProducto:
+            idProductosReparados=manejadorEP.retornaIdProductosReparados()
+            manejadorRP.mostrarProductosNoReparados(idProductosReparados)
+        else:
+            print('Tipo de dato de dato de algun parametro es incorrecto, no se puede llevar a cabo la operacion')
+    def opcion2(self,manejadorRP,manejadorEP):
+        if type(manejadorRP)==ManejadorRecepcionProducto and type(manejadorEP)==ManejadorEntregaProducto:
+            marca=input('Ingrese marca:')
+            idProductos=manejadorRP.BuscarIdProductosPorMarca(marca)
+            if len(idProductos)>0:
+                manejadorEP.calcularMontoTotalPorMarca(idProductos,marca)
+            else:
+                print('No se encontraron productos para la marca ingresada')
+        else:
+            print('Tipo de dato de dato de algun parametro es incorrecto, no se puede llevar a cabo la operacion')
